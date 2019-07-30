a944337f33c98a9e5c6956090b8e708f06b7e4d9
dionesb <diones.breda@gmail.com>
2019-07-30 15:11:10 -0300

    Axios to access API added.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FlatList, Image, Text } from 'react-native';
 
-import { Container } from './styles';
+import api from '../../services/api';
 
-export default function Home() {
-  return <Container />;
+import { Container, ItemProduct } from './styles';
+
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    console.tron.log('componentDidMount');
+    const response = await api.get('/products');
+
+    this.setState({
+      products: response.data,
+    });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <Container>
+        <FlatList
+          data={products}
+          keyExtractor={product => String(product.id)}
+          renderItem={({ item }) => (
+            <ItemProduct>
+              <Image source={{ uri: item.image }} />
+              <Text>{item.title}</Text>
+              <Text>{item.price}</Text>
+            </ItemProduct>
+          )}
+        />
+      </Container>
+    );
+  }
 }
 
 Home.navigationOptions = {
