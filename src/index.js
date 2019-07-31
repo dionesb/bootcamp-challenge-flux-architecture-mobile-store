12a23cbe5d330c3b28903caab720b81a4666705b
dionesb <diones.breda@gmail.com>
2019-07-31 13:58:49 -0300

    Configuring Redux.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+/* Importação do Provider do react-redux para deixa-lo disponível para toda a aplicação. */
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
 
+import store from './store';
+
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#141419" />
       <Routes />
-    </>
+    </Provider>
   );
 }
