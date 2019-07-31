0d18130133866f1055d0cc7aadbedf8a36308fcf
dionesb <diones.breda@gmail.com>
2019-07-31 14:39:12 -0300

    Header amount itens.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Wrapper, Logo, BasketContainer, ItemCount } from './styles';
 
-export default function Header({ navigation }) {
+function Header({ navigation, cartSize }) {
   return (
     <Wrapper>
       <Logo />
       <BasketContainer onPress={() => navigation.navigate('Cart')}>
         <Icon name="shopping-cart" size={24} color="#FFF" />
-        <ItemCount>5</ItemCount>
+        <ItemCount>{cartSize || 0}</ItemCount>
       </BasketContainer>
     </Wrapper>
   );
 }
+
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
