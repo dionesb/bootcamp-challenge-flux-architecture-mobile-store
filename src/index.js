11294dc60bdaefa6987197de9fc0bebe19474127
dionesb <diones.breda@gmail.com>
2019-07-31 23:38:15 -0300

    Saga navigation added.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
-import Routes from './routes';
 
 import store from './store';
+
+import Routes from './routes';
+import NavigationService from './services/navigation';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <StatusBar barStyle="light-content" backgroundColor="#141419" />
-      <Routes />
+      <StatusBar barStyle="light-content" />
+      <Routes
+        ref={navigatorRef => NavigationService.setNavigator(navigatorRef)}
+      />
     </Provider>
   );
 }
